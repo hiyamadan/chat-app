23d8c42be5ad84f2c5b94f26f4579f676a7c4989
Hiya Madan <madan@Dell-Hiya.(none)>
2022-04-24 17:08:58 +0530

    Sharing Location Added

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,7 +6,23 @@ socket.on('Messages',(msg)=>{
 
 document.querySelector('#message-form').addEventListener('submit',(e)=>{
     e.preventDefault()
-    const message = document.querySelector('input').value
+    const message = e.target.elements.message.value
     console.log('Message sent!');
     socket.emit('newMessage',message)
+})
+
+document.querySelector('#send-location').addEventListener('click',()=>{
+    if(!navigator.geolocation)
+    {
+        return alert('Geolocation is not supported by your browser')
+    }
+    navigator.geolocation.getCurrentPosition((position)=>{
+        
+        socket.emit('sendLocation',{
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+    
+    
 })
--- src/index.js
+++ src/index.js
@@ -19,12 +19,20 @@ app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
     console.log('New WebSocket Connection');
-    
-    socket.emit('Messages','Welcome!')
+
+    socket.emit('Messages','Welcome!');
+    socket.broadcast.emit('Messages','A new user has joined!');
 
     socket.on('newMessage',(message)=>{
         io.emit('Messages',message)
     })
+    socket.on('sendLocation',(coords)=>{
+        io.emit('Messages',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+    socket.on('disconnect',()=>{
+        io.emit('Messages','A user has left')
+    })
+    
 })
 
 server.listen(port,()=>{
