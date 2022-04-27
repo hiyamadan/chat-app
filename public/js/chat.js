5e176442fa9680b36dd6a23397ea261eef7f5e4e
Hiya Madan <madan@Dell-Hiya.(none)>
2022-04-27 13:32:54 +0530

    rendering Location Messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,10 +9,17 @@ const $messages = document.querySelector('#messages')
 
 //Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector('#location-template').innerHTML
 
 socket.on('Messages',(msg)=>{
     console.log(msg);
     const html = Mustache.render(messageTemplate,{message:msg})
+    $messages.insertAdjacentHTML('beforeend',html)
+})
+
+socket.on('locationMessage',(url)=>{
+    console.log(url);
+    const html = Mustache.render(locationTemplate,{url})
     $messages.insertAdjacentHTML('beforeend',html)
 })
 
--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ if(filter.isProfane(message)){
         callback()
     })
     socket.on('sendLocation',(coords,callback)=>{
-        io.emit('Messages',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
         callback()
     })
     socket.on('disconnect',()=>{
